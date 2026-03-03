545bbf3201923b507d532e4a12d4b9024c72b25d
Manjunathvpoojari <mm4211051@gmail.com>
2026-03-03 17:13:09 +0530

    firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:first_app/model/counter_model.dart';
 import 'package:first_app/screens/intro_video_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(
     ChangeNotifierProvider(create: (context) => CounterModel(), child: MyApp()),
   );
