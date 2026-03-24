fc179b49487cc0790835951f63d9eeb73f5096b0
Manjunathvpoojari <mm4211051@gmail.com>
2026-03-24 19:51:03 +0530

    main.dart updated with url and keys

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await Supabase.initialize(
-    url: 'sb_publishable_gJJrwVBc5l9GW7hN8rFN7w_kaFqNJlw',
+    url: 'https://zebeimpvhckbylipxiat.supabase.co',
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplYmVpbXB2aGNrYnlsaXB4aWF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyMzYxNTAsImV4cCI6MjA4OTgxMjE1MH0.B2tlP5L2FtJVo7qGfUbcDnyNOOL2Vnrw3wqClKvQ_PM',
   );
